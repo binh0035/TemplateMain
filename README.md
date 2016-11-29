41df4ec844c02245e0c490083427a6775029fa5e
binh <binh1990@126.com>
2016-11-29 13:55:15 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # TemplateMain
 新建App的模板
+
+## 常用类
+- ThisApp Application类
+- ActivityCollector 	用于管理Activity实例
+- BinhActivity	个人所有Activity的父类，以便统一管理
+- SplashActivity	闪的界面
+
+
+
+- SingleToast		连续多按后，不会排队一直显示的Toast
--- app/src/main/java/com/binh/template/templatemain/MainActivity.java
+++ app/src/main/java/com/binh/template/templatemain/MainActivity.java
@@ -3,8 +3,10 @@ package com.binh.template.templatemain;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.binh.template.templatemain.temp.BinhActivity;
+import com.binh.template.templatemain.temp.SingleToast;
 
 public class MainActivity extends BinhActivity {
     private static final String TAG = "MainActivity";
@@ -18,5 +20,12 @@ public class MainActivity extends BinhActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.main_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SingleToast.showToast(MainActivity.this, String.valueOf(BuildConfig.LOG_DEBUG), false);
+            }
+        });
     }
 }
